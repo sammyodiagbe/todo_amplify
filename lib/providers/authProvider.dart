a7f6b42d17568556f1b21772f61b693ffed8fec3
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-10-28 17:11:34 -0500

    creating a user

---
+++ lib/providers/authProvider.dart
@@ -0,0 +1,27 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:flutter/foundation.dart';
+
+class AuthProvider extends ChangeNotifier {
+  bool isAuthenticated = false;
+
+  late AuthUser user;
+
+  Future<void> createUserAccount(
+      String username, String email, String password) async {
+    try {
+      final userAttributes = <CognitoUserAttributeKey, String>{
+        CognitoUserAttributeKey.email: email,
+      };
+
+      final result = await Amplify.Auth.signUp(
+        username: username,
+        password: password,
+        options: CognitoSignUpOptions(userAttributes: userAttributes),
+      );
+      print(result.isSignUpComplete);
+    } catch (err) {
+      safePrint("Something went wrong $err");
+    }
+  }
+}
