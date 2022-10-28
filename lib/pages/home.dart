b77927b0d034a3bda81a1e26823d732f599dc34d
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-10-28 11:25:18 -0500

    learning to initialize amplify properly

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -35,7 +35,7 @@ class _HomeState extends State<Home> {
           child: ElevatedButton(
             child: Text('Click me'),
             onPressed: () {
-              getAuthSession();
+              // getAuthSession();
             },
           ),
         ),
